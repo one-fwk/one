17cd98d2d084212e265478d1debd84a8290b9376
marcus-sa <esetmarcussharkgamingcomputer@gmail.com>
2018-12-13 13:56:46 +0100

    fix: Injector is invalid. Must be an InjectionToken or class annotated with @Injectable()

--- packages/core/src/tokens.ts
+++ packages/core/src/tokens.ts
@@ -1,11 +1,13 @@
 import { Container } from 'inversify';
 
-import { InjectionToken } from './module/injection-token';
+import { InjectionToken } from './module';
 import { Type } from './interfaces';
+import { Injectable } from './decorators';
 
 export const APP_INIT = new InjectionToken<any>('Initialize<App>');
 export const APP_DESTROY = new InjectionToken<any>('Destroy<App>');
 export const MODULE_INIT = new InjectionToken<any>('Initialize<Module>');
-export const ONE_MODULE = new InjectionToken<Type<any>>('Ref<Module>');
+export const MODULE_REF = new InjectionToken<Type<any>>('Ref<Module>');
 
+@Injectable()
 export class Injector extends Container {}
