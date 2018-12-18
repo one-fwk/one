9779b3ff4979ac3f86bca186c46b8721cfe942b7
marcus-sa <esetmarcussharkgamingcomputer@gmail.com>
2018-12-18 16:45:25 +0100

    fix configs

--- packages/testing/rollup.config.js
+++ packages/testing/rollup.config.js
@@ -1,16 +1,1 @@
-import typescript from 'rollup-plugin-typescript2';
-
-export default {
-  input: 'src/index.ts',
-  plugins: [
-    typescript({
-      tsconfig: 'tsconfig.build.json',
-    }),
-  ],
-  output: [
-    {
-      file: 'lib/one-testing.js',
-      format: 'cjs',
-    },
-  ],
-};
+export { default } from '../../rollup.config';
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  treeshake: true,
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.build.json',
+    }),
+  ],
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/index.mjs',
+      format: 'esm',
+    },
+  ],
+};
